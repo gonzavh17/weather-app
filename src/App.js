cecc16a20d0bc03acb044a1fe21ef92c2353a6ef
Gonzalo Asis <gonzaasis17@gmail.com>
2023-03-22 14:00:06 -0300

    fixed resposive bugs

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Footer from './Components/Footer.jsx'
 import Form from './Components/Form.jsx'
 import NavBar from './Components/NavBar.jsx'
 import WeatherPanel from './Components/WeatherPanel.jsx'
+import './Assets/Css/WeatherPanel.css'
 
 function App() {
   return (
-    <div>
+    <div className='app-container'>
       <NavBar/>
       <WeatherPanel/>
       <Footer/>
